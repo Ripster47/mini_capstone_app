a3d44d852000afab52c15587ffe2f1150dd93195
Ripster47 <mpolinski12@gmail.com>
2019-01-22 12:28:57 -0600

    seed dump gem

---
+++ db/old_seeds.rb
@@ -0,0 +1,3 @@
+product = Product.new(name: "Chameleon Twist", price: 90, image_url: "https://www.lukiegames.com/assets/images/N64/n64_chameleon_twist_p_qzqh0f.jpg", description: "Take control of our reptilian hero and use your tongue to eat your way through this delectable adventure!" )
+product.save
+
